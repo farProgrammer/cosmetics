96dea70c2166591bb7a0b7218ed075b651401e6c
farProgrammer <72961644+farProgrammer@users.noreply.github.com>
2021-09-01 22:00:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ db.create_all()
 
  
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 
